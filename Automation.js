9d370d1e56a5bb9f96a4aad42975d8eed4903435
BirajMainali <biraj.prathamit@gmail.com>
2020-09-13 19:43:22 +0545

    No more changes

--- Automation.js
+++ Automation.js
@@ -1,9 +1,14 @@
-function openInChrome(url) {
-    chrome.runtime.sendMessage({ action: "https:/youtube.com", url: url });
+//Auto like/follow/and any task automate
+function WebsiteAutomation(){
+    let Class_Name = prompt("btn class Element Name : ")
+    confirm("Checking ..!")
+    let Action_Name = document.querySelectorAll('.'+Class_Name);
+        if(Action_Name.lenght==0){
+            alert(Class_Name+"is invailed please try next")
+            return false
+        }
+        else{
+            Action_Name.foreach(btn => btn.click())
+        }
 };
-openInChrome(url)
-
-// background
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.action == "https:/youtube.com") window.open(message.url);
-});
+WebsiteAutomation()
--- Raw_source.py
+++ Raw_source.py
@@ -1,2 +1,8 @@
-def source():
-    for i in range(50)
+#follow Algorithm
+def main():
+    People=['1','2','3','4','5','6','8','9','10',
+            '11','12','13','14','15','16','17',
+            '18','19','20','21','23','24','25']
+    for xx in People:
+        print("people count is = "+str(xx)+"...!")
+main()
