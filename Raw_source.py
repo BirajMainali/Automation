986fff29db256235dde3f16a66a4feb650496693
BirajMainali <biraj.prathamit@gmail.com>
2020-09-14 11:55:58 +0545

    Reload source code

--- Raw_source.py
+++ Raw_source.py
@@ -1,11 +1,11 @@
-#follow Algorithm
-# def main():
-#     People=['1','2','3','4','5','6','8','9','10',
-#             '11','12','13','14','15','16','17',
-#             '18','19','20','21','23','24','25']
-#     for xx in People:
-#         print("people count is = "+str(xx)+"...!")
-# main()
+follow Algorithm
+def main():
+    People=['1','2','3','4','5','6','8','9','10',
+            '11','12','13','14','15','16','17',
+            '18','19','20','21','23','24','25']
+    for xx in People:
+        print("people count is = "+str(xx)+"...!")
+main()
 
 def xyx():
     port = "http://xtreme:8000"
