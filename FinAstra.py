774da5ffe4ff281c9dd76777585235fb805f754e
BirajMainali <biraj.prathamit@gmail.com>
2020-09-14 11:52:33 +0545

    init commit after share purcchase compete

--- FinAstra.py
+++ FinAstra.py
@@ -5,15 +5,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
+# import selenium-requests
 
 driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
-# driver.maximize_window()
+driver.maximize_window()
+driver.maximize_window()
 wait = WebDriverWait(driver, 3)
 presence = EC.presence_of_element_located
 visible = EC.visibility_of_element_located
-
-
-driver.get("http://xtreme:8000/Login/Index")
+port = driver.get("http://xtreme:8000/")
 
 def FinAstra_login():
     driver.find_element_by_xpath("//*[@id='UserName']").send_keys("office")
@@ -21,3 +21,18 @@ def FinAstra_login():
     driver.find_element_by_xpath("//*[@id='login']").click()
 
 FinAstra_login()
+
+def Share_purchase():
+    time.sleep(2)
+    driver.get("http://xtreme:8000/Member/BankShare/SharePurchase")
+    driver.find_element_by_xpath("//*[@id='MemberNo_chosen']").click()
+    driver.find_element_by_xpath("//*[@id='MemberNo_chosen']/div/div/input").send_keys("M00001")
+    driver.find_element_by_xpath("//*[@id='MemberNo_chosen']/div/ul/li").click()
+    driver.find_element_by_xpath("//*[@id='ShareNo']").send_keys("10")
+    driver.find_element_by_xpath("//*[@id='MembershipFee']").send_keys("120.5")
+    driver.find_element_by_xpath("//*[@id='Remarks']").send_keys("Share Remarks Test by Auto bot")
+    driver.find_element_by_xpath("//*[@id='sharePurchase']/div/div/div/div[1]/div[6]/div/div/div/button").click()
+    time.sleep(3)
+    alert = driver.switch_to_alert()
+    alert.accept()
+Share_purchase()
